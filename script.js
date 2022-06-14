0b94b37498c0392382e34b296fc324bfc07c0bcb
Sevenpros <sethbisoso@gmail.com>
2022-06-14 05:50:07 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const showMenuBtn = document.querySelector("#show-menu");
+const showMenuBtn = document.querySelector('#show-menu');
 const mobileMenu = document.querySelector('.nav-bar-mobile');
 const navBar = document.querySelector('.nav-bar');
 const cancelMenuBtn = document.querySelector('#cancel-menu');
 const menuList = document.querySelectorAll('.menu-list');
 
-showMenuBtn.addEventListener('click', e => {
-    navBar.classList.toggle('invisible');
-    mobileMenu.classList.toggle('invisible');
+showMenuBtn.addEventListener('click', () => {
+  navBar.classList.toggle('invisible');
+  mobileMenu.classList.toggle('invisible');
 });
-
-cancelMenuBtn.addEventListener('click', e => {
-    navBar.classList.toggle('invisible');
-    mobileMenu.classList.toggle('invisible');
-})
+cancelMenuBtn.addEventListener('click', () => {
+  navBar.classList.toggle('invisible');
+  mobileMenu.classList.toggle('invisible');
+});
 menuList.forEach(list => {
-    list.addEventListener('click',e => {
-        navBar.classList.toggle('invisible');
-        mobileMenu.classList.toggle('invisible');    
-    })
-})
+  list.addEventListener('click', () => {
+    navBar.classList.toggle('invisible');
+    mobileMenu.classList.toggle('invisible');    
+    });
+});
